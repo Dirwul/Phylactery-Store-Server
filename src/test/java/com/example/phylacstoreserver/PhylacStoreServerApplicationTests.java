2cc39a2e81f118682a28a4e838653243e11cbcae
Dirwul <dirwulviques@gmail.com>
2025-05-21 02:55:50 +0300

    initial commit

---
+++ src/test/java/com/example/phylacstoreserver/PhylacStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phylacstoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhylacStoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
