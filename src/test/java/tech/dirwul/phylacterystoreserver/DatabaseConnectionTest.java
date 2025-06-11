f9d74c3e255fcd089ebc0b3906ff78875d37afbc
Dirwul <dirwulviques@gmail.com>
2025-06-11 11:44:41 +0300

    update workflow.yml, remove db tests

--- src/test/java/tech/dirwul/phylacterystoreserver/DatabaseConnectionTest.java
+++ src/test/java/tech/dirwul/phylacterystoreserver/DatabaseConnectionTest.java
@@ -1,30 +1,17 @@
 package tech.dirwul.phylacterystoreserver;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import static org.springframework.test.util.AssertionErrors.assertFalse;
-import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @SpringBootTest
 public class DatabaseConnectionTest {
 
 	private static final Logger log = LoggerFactory.getLogger(DatabaseConnectionTest.class);
 
-	@Autowired
-	private DataSource dataSource;
-
-	private String db_user;
-	private String db_password;
 
 	@BeforeAll
 	static void logging(
@@ -34,18 +21,6 @@ public class DatabaseConnectionTest {
 		log.warn("Autowired:");
 		log.warn("Database user: {}", user);
 		log.warn("Database password: {}", password);
-	}
-
-	@Test
-	void contextLoads() throws SQLException {
-		try (Connection conn = dataSource.getConnection()) {
-			assertFalse("Connection should be open", conn.isClosed());
-		}
-	}
-
-	@Test
-	void testDatabaseConfig() {
-		assertTrue("DB_USER not matching with correct!", db_user.equals("dirwul"));
 	}
 
 }
