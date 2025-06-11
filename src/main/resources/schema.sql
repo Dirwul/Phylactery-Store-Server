6022b1e92e9bbaf88b469ae3c39d7d7d80c6c71e
Dirwul <dirwulviques@gmail.com>
2025-06-11 11:11:01 +0300

    remove initializing sql schema

--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists users (
-    id uuid primary key default gen_random_uuid(),
-    username varchar(255) not null,
-    password varchar(255) not null,
-    token varchar(255) not null unique
-);
